afccabe38eacd1064e76012623e711d1e5327eda
Richard Tom <tomrichard14@gmail.com>
2018-09-09 10:51:09 -0700

    initial folder setup and dependencies

---
+++ utils/seed-db.js
@@ -0,0 +1,25 @@
+// Setting up mongoDB local test data
+
+const mongoose = require('mongoose');
+const {DATABASE_URL} = require('../config');
+
+console.log(`Connecting to mongodb at ${DATABASE_URL}`);
+mongoose.connect(DATABASE_URL)
+  .then(() => {
+    console.info('Dropping Database');
+    return mongoose.connection.db.dropDatabase();
+  })
+  .then(() => {
+    console.info('Seeding Database');
+    return Promise.all([
+
+    ]);
+  })
+  .then(() => {
+    console.info('Disconnecting');
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(err);
+    return mongoose.disconnect();
+  });
