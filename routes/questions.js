14bebf94b2bf920e2f6ecff4440f06872e61b94f
Akim Desire <adesi006.ng@gmail.com>
2018-09-12 15:05:08 -0400

    records moved to user object

--- routes/questions.js
+++ routes/questions.js
@@ -1,56 +1,58 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const mongoose = require('mongoose');
-const passport = require('passport');
-const Question = require('../models/question');
-const User = require('../models/user');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const Question = require("../models/question");
+const User = require("../models/user");
 
-const jwtAuth = passport.authenticate('jwt', { session: false });
+const jwtAuth = passport.authenticate("jwt", { session: false });
 
+// router.get('/', (req, res, next) => {
+//   Question.aggregate([ { $sample: { size: 1 } } ])
+//     .then(question => res.json(question[0]))
+//     .catch(err => next(err));
+// });
 
-router.get('/', (req, res, next) => {
-  Question.aggregate([ { $sample: { size: 1 } } ])
-    .then(question => res.json(question[0]))
-    .catch(err => next(err));
-});
-
-router.get('/', jwtAuth, (req, res, next) => {
+router.get("/", jwtAuth, (req, res, next) => {
   const userId = req.user.id;
 
   if (!mongoose.Types.ObjectId.isValid(userId)) {
-    const err = new Error('The `userId` is not valid');
+    const err = new Error("The `userId` is not valid");
     err.status = 400;
     return next(err);
   }
 
-  User.findOne({_id: userId})
+  User.findOne({ _id: userId })
     .then(result => result.questions[result.head])
     .then(data => res.json(data))
     .catch(err => next(err));
 });
 
-router.post('/', jwtAuth, (req, res, next) => {
+router.post("/", jwtAuth, (req, res, next) => {
   const userId = req.user.id;
-  const {correct} = req.body;
+  const { correct } = req.body;
   let answeredHead;
   let answeredNode;
-  User.findOne({_id: userId})
+  User.findOne({ _id: userId })
     .then(user => {
       answeredHead = user.head;
       answeredNode = user.questions[answeredHead];
       // nextHead = user.questions.indexOf(user.questions[user.questions[answeredHead].next]);
       // console.log('answeredNode', answeredNode);
-      correct ? answeredNode.memoryStrength *= 2 : answeredNode.memoryStrength = 1;
-      
+      correct
+        ? (answeredNode.memoryStrength *= 2)
+        : (answeredNode.memoryStrength = 1);
+      correct ? (answeredNode.correct += 1) : (answeredNode.incorrect += 1);
+
       user.head = answeredNode.next;
       let nextNode = answeredNode;
-      for (let i = 0; i < answeredNode.memoryStrength+1; i++) {
+      for (let i = 0; i < answeredNode.memoryStrength + 1; i++) {
         nextNode = user.questions[nextNode.next];
       }
 
       if (answeredNode.memoryStrength > user.questions.length) {
-        nextNode = user.questions[user.questions.length-1];
+        nextNode = user.questions[user.questions.length - 1];
       }
       // console.log('answeredNode', answeredNode);
       // console.log('resultNode ', result.questions[answeredHead]);
@@ -61,7 +63,9 @@ router.post('/', jwtAuth, (req, res, next) => {
       nextNode.next = answeredHead;
       return user.save();
     })
-    .then(result => User.findOneAndUpdate({_id: userId}, result, {new:true}))
+    .then(result =>
+      User.findOneAndUpdate({ _id: userId }, result, { new: true })
+    )
     .then(result => {
       if (result) {
         res.json(result);
@@ -70,7 +74,6 @@ router.post('/', jwtAuth, (req, res, next) => {
       }
     })
     .catch(err => next(err));
-
 });
 
 module.exports = router;
