c25b1afac242cb600e187edcdfe98b7829fd59d7
Richard Tom <tomrichard14@gmail.com>
2018-09-13 12:00:20 -0700

    comment out

--- index.js
+++ index.js
@@ -29,16 +29,16 @@ app.use(express.json());
 //     allowedHeaders:['Content-Type','Authorization']
 //   })
 // );
-app.use((req, res, next) => { 
-  res.header('Access-Control-Allow-Origin', '*'); 
-  res.header('Access-Control-Allow-Credentials','true'); 
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization'); 
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE'); 
-  if(req.method === 'OPTIONS') { 
-    return res.sendStatus(204); 
-  } 
-  return next();
-});
+// app.use((req, res, next) => { 
+//   res.header('Access-Control-Allow-Origin', '*'); 
+//   res.header('Access-Control-Allow-Credentials','true'); 
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization'); 
+//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE'); 
+//   if(req.method === 'OPTIONS') { 
+//     return res.sendStatus(204); 
+//   } 
+//   return next();
+// });
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
