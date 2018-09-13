36c8fa3b57bfd903b6265a2cfe21262bc02b37f7
Richard Tom <tomrichard14@gmail.com>
2018-09-12 17:57:58 -0700

    deploy

--- index.js
+++ index.js
@@ -22,15 +22,19 @@ app.use(
     skip: (req, res) => process.env.NODE_ENV === "test"
   })
 );
-
-app.use(
-  cors({
-    origin: '*',
-    allowedHeaders:['Content-Type','Authorization']
-  })
-);
-
 app.use(express.json());
+// app.use(
+//   cors({
+//     allowedHeaders:['Content-Type','Authorization']
+//   })
+// );
+app.use((req, res, next) => { 
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Credentials','true'); 
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization'); 
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE'); 
+  if(req.method === 'OPTIONS') { return res.sendStatus(204); } return next();
+});
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
